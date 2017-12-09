29dda810bd5df81ff37d42581d17e72fa26d399e
Minnie <na.minnie.190839@hotmail.com>
2017-12-10 02:12:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/win8/myapplication/PicListAdapter.java
@@ -0,0 +1,39 @@
+package com.example.win8.myapplication;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by MSI-GL62 on 9/12/2560.
+ */
+
+public class PicListAdapter extends ArrayAdapter<PictureItem> {
+    private Context mContext;
+    private int mLayoutResId;
+    private ArrayList<PictureItem> mPicList;
+
+    public PicListAdapter(Context mContext, int mLayoutResId, ArrayList<PictureItem> mPicList) {
+        super(mContext,mLayoutResId,mPicList);
+        this.mContext = mContext;
+        this.mLayoutResId = mLayoutResId;
+        this.mPicList = mPicList;
+    }
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View itemLayout = inflater.inflate(mLayoutResId, null);
+        PictureItem item = mPicList.get(position);
+        ImageView picImageView = itemLayout.findViewById(R.id.imageView);
+        picImageView.setImageResource(item.picture);
+        return itemLayout;
+    }
+}
---
+++ app/src/main/java/com/example/win8/myapplication/PictureItem.java
@@ -0,0 +1,19 @@
+package com.example.win8.myapplication;
+
+/**
+ * Created by Win8 on 10/12/2560.
+ */
+
+public class PictureItem {
+
+    public final int id;
+    public final String type;
+    public final int picture;
+
+    public PictureItem(int id, String type, int picture) {
+        this.id = id;
+        this.type = type;
+        this.picture = picture;
+    }
+
+}
